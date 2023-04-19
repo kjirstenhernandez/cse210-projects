70c7212cab326501a894e7145e1bdecedb033fab
kjirstenhernandez <sandlot7665@gmail.com>
2023-04-18 21:10:06 -0600

    Finished Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,12 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Ask the user for their name
         Console.Write("What is your first name? ");
         string first_name = Console.ReadLine();
 
         Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
+
+        // Blank Line to match example
         Console.WriteLine("");
+
+        // "Bond, James Bond" Statement with user input
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
