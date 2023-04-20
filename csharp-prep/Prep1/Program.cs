3f6fe13e13d007827410b60a475e7be3d361408b
kjirstenhernandez <sandlot7665@gmail.com>
2023-04-19 20:01:08 -0600

    Made changes to variables for Camel Casing

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,15 +6,15 @@ class Program
     {
         //Ask the user for their name
         Console.Write("What is your first name? ");
-        string first_name = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string last_name = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
         // Blank Line to match example
         Console.WriteLine("");
 
         // "Bond, James Bond" Statement with user input
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
