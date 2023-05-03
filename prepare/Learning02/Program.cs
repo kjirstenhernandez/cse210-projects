ae87d09a9d8acc6bdc51a797d826bdec71a83895
kjirstenhernandez <sandlot7665@gmail.com>
2023-05-02 21:12:30 -0600

    Completed Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Developer";
+        job1._endYear = 2020;
+        job1._startYear = 2018;
+        
+
+        Job job2 = new Job();
+        job2._company = "Wavetronics";
+        job2._jobTitle = "Team Lead - Software Developer";
+        job2._endYear = 2023;
+        job2._startYear = 2020;
+        
+
+        Resume myResume = new Resume();
+        myResume._name = "Sarah Parker";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        myResume.DisplayResume();
+
     }
 }
