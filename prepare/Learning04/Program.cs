0d0ebea2708f1b83b16ec0342a3a3d31137432e1
kjirstenhernandez <sandlot7665@gmail.com>
2023-05-31 22:35:26 -0600

    Completed Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment newAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(newAssignment.GetSummary());
+
+        MathAssignment newMathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(newMathAssignment.GetSummary());
+        Console.WriteLine(newMathAssignment.GetHomeworkList());
+
+        WritingAssignment newWritingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of WWII");
+        Console.WriteLine(newWritingAssignment.GetSummary());
+        Console.WriteLine(newWritingAssignment.GetWritingInformation());
     }
 }
