dec567bdfd9376c5862b11d0836ae50db3c9b452
kjirstenhernandez <sandlot7665@gmail.com>
2023-06-14 17:01:43 -0600

    Completed Learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Test Square
+        Square square = new Square("red", 3.12);
+        Console.WriteLine(square.GetArea());
+        Console.WriteLine(square.GetColor());
+
+        // Test Rectangle
+        Rectangle rectangle = new Rectangle("yellow", 2, 4);
+        Console.WriteLine(rectangle.GetArea());
+        Console.WriteLine(rectangle.GetColor());
+
+        //Test Circle
+        Circle circle = new Circle("purple", .5);
+        Console.WriteLine(circle.GetArea());
+        Console.WriteLine(circle.GetColor());
+
+        // Iterate through list of various shapes
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("green", 4));
+        shapes.Add(new Rectangle("red", 2, 4));
+        shapes.Add(new Circle("blue", 5));
+
+        foreach (Shape shape in shapes){
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+        }
+
     }
 }
